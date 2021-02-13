be1a89e033d1381046e83c1bd3bfbad9161bc8e6
JT-love-WJ <79003578+JT-love-WJ@users.noreply.github.com>
2021-02-13 19:17:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var config = {
         "Sweet heart",
         "Loving you is the important thing.",
         "There are some people who think love is sex and marriage,",
-        "and perhaps it is,".
+        "and perhaps it is.",
         "But do you know what I think?",
-        "I think love is sharing a lifetime with you".
-        "Love you FOREVER"
+        "I think love is sharing a lifetime with you.",
+        "Love you FOREVER.",
     ],
     date: "2019-08-15T20:15:00",
     names: ["Jingtao ZHANG", "Wenjie YI"]
