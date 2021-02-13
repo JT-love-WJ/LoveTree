ce85a1e997502fab36588cf7eb3ad79e410c0b51
JT-love-WJ <79003578+JT-love-WJ@users.noreply.github.com>
2021-02-13 19:24:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     lines: [
         "Sweet heart",
         "Loving you is the important thing.",
-        "There are some people who think love is sex and marriage,",
+        "Some people think love is sex and marriage,",
         "and perhaps it is.",
         "But do you know what I think?",
         "I think love is sharing a lifetime with you.",
